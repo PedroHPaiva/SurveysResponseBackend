950ffffc2adaedf80059cb91babe56f5ed629962
Pedro Paiva <pedro.paiva@somagrupo.com.br>
2025-04-28 03:37:35 -0300

    tentativa de acesso ao banco

--- src/database/inside.ts
+++ src/database/inside.ts
@@ -5,6 +5,13 @@ dotenv.config();
 export const insideDatabase = new Sequelize(process.env.DB_NAME ?? "", process.env.DB_USER ?? "", process.env.DB_PASS ?? "", {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+  dialectOptions: {
+    family: 4,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   dialect: "postgres",
   logging: false,
 });
